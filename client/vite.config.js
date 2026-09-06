3ca333d2921878e9ca1c5414e8626e5f7f7de5fb
Neeraj Giri <neerajgoswami951@gmail.com>
2026-09-06 23:30:27 +0530

    Fix Render preview host

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
     proxy: {
@@ -13,5 +14,11 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['studentos-client.onrender.com'],
   },
 });
